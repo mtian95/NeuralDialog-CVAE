d827c929e880b9e4b19b5232832f6ee2bcb236e2
mtian95 <mtian258@gmail.com>
2018-03-27 11:13:34 +0800

    Reformatted data

--- data_apis/data_utils.py
+++ data_apis/data_utils.py
@@ -28,8 +28,8 @@ class LongDataLoader(object):
     def epoch_init(self, batch_size, backward_size, step_size, shuffle=True, intra_shuffle=True):
         assert len(self.indexes) == self.data_size and len(self.data_lens) == self.data_size
 
-        self.ptr = 0
-        self.batch_size = batch_size
+        self.ptr = 0 # pointer
+        self.batch_size = batch_size # currently set to 30 in config
         self.backward_size = backward_size
         self.step_size = step_size
         self.prev_alive_size = batch_size
@@ -37,8 +37,10 @@ class LongDataLoader(object):
         # create batch indexes
         temp_num_batch = self.data_size // batch_size
         self.batch_indexes = []
+        # batch_indexes = list of lists of the indices in each batch. one list for each batch. 
         for i in range(temp_num_batch):
             self.batch_indexes.append(self.indexes[i * self.batch_size:(i + 1) * self.batch_size])
+
 
         left_over = self.data_size-temp_num_batch*batch_size
 
@@ -48,14 +50,18 @@ class LongDataLoader(object):
 
         # create grid indexes
         self.grid_indexes = []
+        # idx is the batch number, b_ids is a list of all the data indices in this batch
         for idx, b_ids in enumerate(self.batch_indexes):
             # assume the b_ids are sorted
-            all_lens = [self.data_lens[i] for i in b_ids]
-            max_len = self.data_lens[b_ids[-1]]
+            all_lens = [self.data_lens[i] for i in b_ids] # lengths of the dialogs in this batch 
+            max_len = self.data_lens[b_ids[-1]] 
             min_len = self.data_lens[b_ids[0]]
-            assert np.max(all_lens) == max_len
+            assert np.max(all_lens) == max_len # max_len = max number of sentences in a dialog in this batch
             assert np.min(all_lens) == min_len
-            num_seg = (max_len-self.backward_size) // self.step_size
+
+            # segment start and end indicies. Each segment should be of len backward_size if possible (sometimes shorter)
+            num_seg = (max_len-self.backward_size) // self.step_size 
+            # print("number of segments " + str(num_seg))
             if num_seg > 0:
                 cut_start = range(0, num_seg*self.step_size, step_size)
                 cut_end = range(self.backward_size, num_seg*self.step_size+self.backward_size, step_size)
@@ -70,7 +76,6 @@ class LongDataLoader(object):
             if intra_shuffle and shuffle:
                np.random.shuffle(new_grids)
             self.grid_indexes.extend(new_grids)
-
 
         self.num_batch = 1. #len(self.grid_indexes) #TODO fix this
         print("%s begins with %d batches with %d left over samples" % (self.name, self.num_batch, left_over))
@@ -89,19 +94,24 @@ class LongDataLoader(object):
 
 
 class SWDADataLoader(LongDataLoader):
-    def __init__(self, name, data, meta_data, config):
+    # TODO change this none
+    def __init__(self, name, data, meta_data, config, lda_model=None):
         assert len(data) == len(meta_data)
         self.name = name
-        self.data = data
-        self.meta_data = meta_data
+        self.data = data # from api.get_dialog_corpus()
+        self.meta_data = meta_data # from api.get_meta_corpus()
         self.data_size = len(data)
+        self.lda_model = lda_model
+        # length of each dialog aka num of sentences
         self.data_lens = all_lens = [len(line) for line in self.data]
         self.max_utt_size = config.max_utt_len
         print("Max len %d and min len %d and avg len %f" % (np.max(all_lens),
                                                             np.min(all_lens),
                                                             float(np.mean(all_lens))))
+        # self.indexes gets a list of indices that would sort the dialogs in increasing order of sent number
         self.indexes = list(np.argsort(all_lens))
 
+    # pads tokens to be max_utt_size with the "pad" character in corpus
     def pad_to(self, tokens, do_pad=True):
         if len(tokens) >= self.max_utt_size:
             return tokens[0:self.max_utt_size-1] + [tokens[-1]]
@@ -110,16 +120,36 @@ class SWDADataLoader(LongDataLoader):
         else:
             return tokens
 
+    """
+        Extracts all the info for a grid (aka a segment) in a batch.
+        A grid is a batch of data that only looks at a segment of the dialogs in each batch. 
+        b_id = index of the batch being prepared
+        s_id = starting point of segment within a dialog (ex: take the first 5 sentences in dialog to predict the 6th)
+        e_id = ending point of segment within a dialog 
+
+        batch_indexes = list of lists of the data indices in each batch
+        batch_ids = list of all data indices included in batch b_id
+        rows = list of dialogs in this batch. from api.get_dialog_corpus()
+            Each dialog is a list of sentences. 
+            Each sentence = (sent in tokens, 0/1 paragraph end label, vector version of topic of just that sent)
+        meta_rows = list of meta info for the data in this batch. from api.get_meta_corpus()
+            Each paragraph has its own meta data = topic of that entire paragraph
+        dialog_lens = list of number of sentences in each dialog in this batch
+
+        out_row = sentence being predicted using in_row as context
+        floors = list of lists of paragraph end labels (1 if sent is last in par, else 0)
+        out_das = sentence topic of the sentence being predicted (out_row)
+    """
     def _prepare_batch(self, cur_grid, prev_grid):
-        # the batch index, the starting point and end point for segment
         b_id, s_id, e_id = cur_grid
+        print "starting id", s_id
 
         batch_ids = self.batch_indexes[b_id]
         rows = [self.data[idx] for idx in batch_ids]
         meta_rows = [self.meta_data[idx] for idx in batch_ids]
         dialog_lens = [self.data_lens[idx] for idx in batch_ids]
 
-        topics = np.array([meta[2] for meta in meta_rows])
+        # topics = np.array([meta[2] for meta in meta_rows])
         cur_pos = [np.minimum(1.0, e_id/float(l)) for l in dialog_lens]
 
         # input_context, context_lens, floors, topics, a_profiles, b_Profiles, outputs, output_lens
@@ -127,26 +157,30 @@ class SWDADataLoader(LongDataLoader):
         for row in rows:
             if s_id < len(row)-1:
                 cut_row = row[s_id:e_id]
+
+                # split rows into input (in_row) and the thing being predicted (out_row)
                 in_row = cut_row[0:-1]
                 out_row = cut_row[-1]
-                out_utt, out_floor, out_feat = out_row
+                out_utt, out_floor, out_sent_topic = out_row
 
-                context_utts.append([self.pad_to(utt) for utt, floor, feat in in_row])
-                floors.append([int(floor==out_floor) for utt, floor, feat in in_row])
+                context_utts.append([self.pad_to(utt) for utt, floor, sent_topic in in_row])
+                floors.append([floor for utt, floor, sent_topic in in_row])
+                # floors.append([int(floor==out_floor) for utt, floor, sent_topic in in_row])
                 context_lens.append(len(cut_row) - 1)
 
                 out_utt = self.pad_to(out_utt, do_pad=False)
                 out_utts.append(out_utt)
                 out_lens.append(len(out_utt))
                 out_floors.append(out_floor)
-                out_das.append(out_feat[0])
+                out_das.append(out_sent_topic)
+
             else:
                 print(row)
                 raise ValueError("S_ID %d larger than row" % s_id)
 
-        # my_profiles = np.array([meta[out_floors[idx]] + [cur_pos[idx]] for idx, meta in enumerate(meta_rows)])
-        my_profiles = np.array([meta[out_floors[idx]] for idx, meta in enumerate(meta_rows)])
-        ot_profiles = np.array([meta[1-out_floors[idx]] for idx, meta in enumerate(meta_rows)])
+
+        # my_profiles = np.array([meta[out_floors[idx]] for idx, meta in enumerate(meta_rows)])
+        # ot_profiles = np.array([meta[1-out_floors[idx]] for idx, meta in enumerate(meta_rows)])
         vec_context_lens = np.array(context_lens)
         vec_context = np.zeros((self.batch_size, np.max(vec_context_lens), self.max_utt_size), dtype=np.int32)
         vec_floors = np.zeros((self.batch_size, np.max(vec_context_lens)), dtype=np.int32)
@@ -159,7 +193,9 @@ class SWDADataLoader(LongDataLoader):
             vec_floors[b_id, 0:vec_context_lens[b_id]] = floors[b_id]
             vec_context[b_id, 0:vec_context_lens[b_id], :] = np.array(context_utts[b_id])
 
-        return vec_context, vec_context_lens, vec_floors, topics, my_profiles, ot_profiles, vec_outs, vec_out_lens, vec_out_das
+        # return vec_context, vec_context_lens, vec_floors, topics, my_profiles, ot_profiles, vec_outs, vec_out_lens, vec_out_das
+        return vec_context, vec_context_lens, vec_floors, vec_outs, vec_out_lens, vec_out_das
+
 
 
 
